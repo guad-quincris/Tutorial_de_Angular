3d6b369fdd78e64c1a33a5aee165663cb3f0e6bf
Gustavo Quincris <guad.quincrisdes@gmail.com>
2020-02-12 17:18:13 -0500

    CRUD tutorial de angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
+
+import {FormsModule} from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: ':status', component: TodoComponent },
+  { path: '**', redirectTo: '/all' }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
